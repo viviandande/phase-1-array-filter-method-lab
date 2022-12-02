4a0ef21714575f8f97c5e2d710d59e8fce9c8dcc
+ <+>
2022-12-02 11:57:59 +0300

    Completed assignmet

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(drivers => drivers.toLowerCase() === string.toLowerCase())
+
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(drivers => drivers.startsWith(string))
+
+}
+
+function matchName (drivers, string) {
+    return drivers.filter( drivers => drivers.name === string)
+}
